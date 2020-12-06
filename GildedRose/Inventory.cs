d5571030ef35465d727ea9495beebe2dd3408a52
martinorion <71751917+martinorion@users.noreply.github.com>
2020-12-06 15:44:42 +0100

    Update Inventory.cs

--- GildedRose/Inventory.cs
+++ GildedRose/Inventory.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
+using System;
 
 namespace GildedRose
 {
@@ -11,19 +12,51 @@ namespace GildedRose
             this.items = items;
         }
 
-        /// <summary>
-        /// Items:
-        /// "+5 Dexterity Vest"
-        /// "Aged Brie"
-        /// "Elixir of the Mongoose"
-        /// "Sulfuras, Hand of Ragnaros"
-        /// "Backstage passes to a TAFKAL80ETC concert"
-        /// "Conjured Mana Cake"
-        /// </summary>
         public void UpdateQuality()
         {
-            // TODO ...
-            // Hint: Iterate through this.items and check Name property to access specific item
+
+            foreach (var item in items)
+            {
+                if (item.Name == "Sulfuras, Hand of Ragnaros")
+                {
+                    item.Quality = Math.Clamp(item.Quality, 0, 100);
+                }
+                else
+                {
+                    item.SellIn--;
+                    int decrement = (item.SellIn <= 0 ? 2 : 1);
+                    if (item.Name == "Aged Brie")
+                    {
+                        item.Quality += decrement;
+                    }
+                    else if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
+                    {
+
+                        if (item.SellIn <= 10 && item.SellIn > 5)
+                            item.Quality += 2;
+                        else if (item.SellIn <= 5 && item.SellIn > 0)
+                            item.Quality += 3;
+                        else
+                            item.Quality++;
+
+
+                        if (item.SellIn <= 0)
+                            item.Quality = 0;
+
+                    }
+                    else if (item.Name == "Conjured Mana Cake")
+                    {
+                        item.Quality -= decrement * 2;
+                    }
+                    else
+                    {
+                        item.Quality -= decrement;
+                    }
+                    item.Quality = Math.Clamp(item.Quality, 0, 50);
+
+
+                }
+            }
         }
     }
 }
